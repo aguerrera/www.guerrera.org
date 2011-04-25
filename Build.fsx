498bd6883322600b57dda4f759c68e5b457cbb61
Andy Guerrera <aguerrera@yahoo.com>
2011-04-24 22:38:50 -0400

    setup

---
+++ Build.fsx
@@ -0,0 +1,12 @@
+﻿
+#r "lib\Garoozis.dll"
+
+open System
+open System.Collections.Generic
+open System.IO
+
+
+let configSource = "config.js"
+let config = Garoozis.Utils.get_config(configSource)
+Garoozis.Transformer.build_pages config.OutputDir config.SourceDir
+Garoozis.RemoteStorage.publish_to_s3(config)
