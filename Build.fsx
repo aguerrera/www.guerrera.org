598ba849dae2c9d44d3df1fd81424f092e5176b6
Andy Guerrera <aguerrera@yahoo.com>
2011-04-25 00:24:02 -0400

    getting there.

--- Build.fsx
+++ Build.fsx
@@ -1,12 +1,14 @@
 ﻿
-#r "lib\Garoozis.dll"
+#I @"c:\source\ionchamp\Garoozis\Garoozis\bin\debug"
+#r "Garoozis.dll"
 
 open System
 open System.Collections.Generic
 open System.IO
+open Garoozis
 
 
-let configSource = "config.js"
+let configSource = __SOURCE_DIRECTORY__ + @"\config.js"
 let config = Garoozis.Utils.get_config(configSource)
 Garoozis.Transformer.build_pages config.OutputDir config.SourceDir
 Garoozis.RemoteStorage.publish_to_s3(config)
