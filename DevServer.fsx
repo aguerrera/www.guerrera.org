882b63a2c77c87554a50c884ba64bd608c9b0829
Andy Guerrera <aguerrera@yahoo.com>
2011-04-27 15:14:16 -0400

    pictures

---
+++ DevServer.fsx
@@ -0,0 +1,21 @@
+﻿
+#I @"c:\source\ionchamp\Garoozis\Garoozis\bin\debug"
+#r "Garoozis.dll"
+
+open System
+open System.Collections.Generic
+open System.IO
+open Garoozis
+
+
+let configSource = __SOURCE_DIRECTORY__ + @"\config.js"
+let config = Garoozis.Utils.get_config(configSource)
+
+printfn "STARTING output server www.guerrera.org."
+Garoozis.WebServer.StartOutputServer 8088 config
+
+//printfn "STARTING dev server www.guerrera.org."
+//Garoozis.WebServer.StartDevServer 8088 config
+
+
+Console.ReadKey()
