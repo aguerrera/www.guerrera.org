5128ab5c1de48319854fff1448483216513856e8
Andy Guerrera <aguerrera@yahoo.com>
2011-04-27 16:15:27 -0400

    dev and static server

--- DevServer.fsx
+++ DevServer.fsx
@@ -11,11 +11,21 @@ open Garoozis
 let configSource = __SOURCE_DIRECTORY__ + @"\config.js"
 let config = Garoozis.Utils.get_config(configSource)
 
-printfn "STARTING output server www.guerrera.org."
-Garoozis.WebServer.StartOutputServer 8088 config
-
-//printfn "STARTING dev server www.guerrera.org."
-//Garoozis.WebServer.StartDevServer 8088 config
+let args = Environment.GetCommandLineArgs()
 
 
-Console.ReadKey()
+let run_output_server () = 
+    printfn "STARTING output server www.guerrera.org."
+    Garoozis.WebServer.StartOutputServer 8088 config
+
+let run_dev_server () = 
+    printfn "STARTING dev server www.guerrera.org."
+    Garoozis.WebServer.StartDevServer 8088 config
+
+if args.Length > 0 then
+    let isstatic = args |> Array.exists (fun s -> s.ToLower() = "--static")
+    match isstatic with
+    | true -> run_output_server()
+    | _ -> run_dev_server()
+else
+    run_dev_server () 
